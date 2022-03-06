3ddb3da4a5a1080dbdf5acecfe3f73ac8d26f61e
Dnyaneshwar madake <87903257+DnyaneshwarMadake@users.noreply.github.com>
2022-03-06 16:25:45 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+#Add implemnetation
+def add(x,y):
+    return(x+y)
+#subb implemnetation
+def subb(x,y):
+    pass
+#mul implemnetation  
+def mul(x,y):
+    pass
+#div implemnetation  
+def div(x,y):
+    if y==0:
+        return dived by 0
+    else:
+        return x/y
+        
+#Square implemntation
+def square(x):
+    return x*x
